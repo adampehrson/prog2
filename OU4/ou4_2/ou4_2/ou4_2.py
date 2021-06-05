415607f4a379a5afac9333d46e748bf0c6ac3a66
Adam Pehrson <adam.elis.pehrson@gmail.com>
2021-06-05 22:39:45 +0200

    gd

--- OU4/ou4_2/ou4_2/ou4_2.py
+++ OU4/ou4_2/ou4_2/ou4_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 from heltal import Heltal
-
+from time import perf_counter
 
 def main():
 	f = Heltal(5)
@@ -17,12 +17,24 @@ def fib_py(n):
 		return n
 	else:
 		return fib_py(n-1) + fib_py(n-2)
+	
 
 def fib_c(f):
 	return f.fib()
 
 if __name__ == '__main__':
-	print(fib_py(10))
+	value = 10
 
-	f = Heltal(10)
+
+	start_fib_py = perf_counter
+	print(fib_py(value))
+	end_fib_py = perf_counter
+	print(f"Python process took {round(start_fib_py - end_fib_py, 2)} seconds")
+
+	f = Heltal(value)
+
+	start_fib_c = perf_counter
 	print(f.fib())
+	end_fib_c = perf_counter
+	print(f"c++ process took {round(start_fib_c - end_fib_c, 2)} seconds")
+
