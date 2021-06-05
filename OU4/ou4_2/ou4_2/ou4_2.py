3a8ef93bfe5f0cec52b5509c5742fb2ef79264e6
Adam Pehrson <adam.elis.pehrson@gmail.com>
2021-06-05 23:06:17 +0200

    sdfa

--- OU4/ou4_2/ou4_2/ou4_2.py
+++ OU4/ou4_2/ou4_2/ou4_2.py
@@ -2,27 +2,9 @@
 
 from heltal import Heltal
 from time import perf_counter
+import matplotlib.pyplot as plt
 
 def main():
-	f = Heltal(5)
-	print(f.get())
-	f.set(7)
-	print(f.get())
-
-
-
-
-def fib_py(n):
-	if n <= 1:
-		return n
-	else:
-		return fib_py(n-1) + fib_py(n-2)
-	
-
-def fib_c(f):
-	return f.fib()
-
-if __name__ == '__main__':
 	value = 35
 
 
@@ -38,3 +20,41 @@ if __name__ == '__main__':
 	end_fib_c = perf_counter()
 	print(f"c++ process took {round(end_fib_c - start_fib_c, 2)} seconds")
 
+
+def timetaking(lower_bound, upper_bound):
+	data = []
+	for x in range(lower_bound, upper_bound):
+		start = perf_counter()
+		fib_py(x)
+		end = perf_counter
+
+		f = Heltal(x)
+
+		newstart = perf_counter()
+		fib_c(f)
+		newend = perf_counter()
+
+		data.append((x, round(end - start, 2), round(newend - newstart, 2)))
+	plt.scatter(data[0], data[1], c="b", label="Only Python")
+	plt.scatter(data[0], data[2], c="r", label="Running in c++")
+	plt.legend(loc="upper left")
+	plt.savefig("Fib_times.png")
+
+
+def fib_py(n):
+	if n <= 1:
+		return n
+	else:
+		return fib_py(n-1) + fib_py(n-2)
+	
+
+def fib_c(f):
+	return f.fib()
+
+
+
+
+if __name__ == '__main__':
+	#main()
+
+	timetaking(10,33)
